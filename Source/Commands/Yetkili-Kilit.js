a4f16f28e1d42d5eabba52291099ba9850321666
Lûcy <82613979+Patavatsiz@users.noreply.github.com>
2021-06-30 10:45:01 +0300

    Update Yetkili-Kilit.js

--- Source/Commands/Yetkili-Kilit.js
+++ Source/Commands/Yetkili-Kilit.js
@@ -27,10 +27,10 @@ module.exports = {
     if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.wsend(embed.setDescription("Hata: Bu komudu kullanamazsın")).sil(7);
     var ever = message.guild.roles.cache.find(evry => evry.name === "@everyone");
     if (args[0] && args[0].toLocaleLowerCase().includes('aç' || 'on')) {
-    message.channel.updateOverwrite(ever, { 'SEND_MESSAGES': null })
+    message.channel.updateOverwrite(ever, { 'SEND_MESSAGES': false })
     message.react(lucy.emojiler.yes);
     } else if (args[0] && args[0].toLocaleLowerCase().includes('kapat' || 'off')) {
-      message.channel.updateOverwrite(ever, { 'SEND_MESSAGES': true })
+      message.channel.updateOverwrite(ever, { 'SEND_MESSAGES': null })
       message.react(lucy.emojiler.yes);
     } else if(!args[0]) return message.channel.wsend(embed.setDescription(`Hata: bir işlem belirtin => .kilit aç/on - kapat/off`)).sil(7);
   }
